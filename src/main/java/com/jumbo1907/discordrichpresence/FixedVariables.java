0ebbe20ed9c5ab5e40f5ff22909d1862e7e84790
Jumbo1907 <jumbo1907.primemc@gmail.com>
2019-06-09 13:16:28 +0200

    Create Fixed Variables

---
+++ src/main/java/com/jumbo1907/discordrichpresence/FixedVariables.java
@@ -0,0 +1,25 @@
+package com.jumbo1907.discordrichpresence;
+
+public class FixedVariables {
+
+
+    private static final String version = "0.0.1";
+    /*
+    GUI related
+     */
+
+    public static final int GUI_WIDTH = 1200;
+    public static final int GUI_HEIGHT = 600;
+    public static final String GUI_NAME = "Presence Producer v" + version;
+
+    //Startup animation (ms)
+    public static final int ANIMATION_LENGTH = 1000;
+    public static final int ANIMATION_DELAY = 1000;
+
+    //Colors
+    public static final String COLOR_DARK = "#2C2F33";
+    public static final String COLOR_ORANGE = "#ea5e00";
+
+
+
+}
